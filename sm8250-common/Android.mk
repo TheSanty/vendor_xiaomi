3d21248867a43fcb9ddb77029725bbedeebfe587
Ramii Ahmed <ramy@ahmedramy.com>
2021-06-17 15:01:34 +0000

    apollon: initial import * From apollo-user 11 RKQ1.200826.002 V12.1.3.0.RJKMIXM release-keys

--- sm8250-common/Android.mk
+++ sm8250-common/Android.mk
@@ -4,6 +4,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alioth apollo cas cmi lmi thyme umi,$(TARGET_DEVICE)),)
+ifneq ($(filter alioth apollon cas cmi lmi thyme umi,$(TARGET_DEVICE)),)
 
 endif
